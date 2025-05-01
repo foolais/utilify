27301322bfaae3c4f8fe667fb156e6b482bbf08b
Wahyu Esya <wahyu.esya17@gmail.com>
2025-05-02 00:08:09 +0700

    feat(LoanList) add table skeleton loading

--- src/app/(admin)/admin/loans-list/page.tsx
+++ src/app/(admin)/admin/loans-list/page.tsx
@@ -1,5 +1,6 @@
 import ContainerSearchForm from "@/components/container/container-search-form";
 import DialogCreateLoansList from "@/components/dialog/dialog-create-loans-list";
+import { TableLoadingSkeleton } from "@/components/skeleton/loading-skeleton";
 import { DataTable } from "@/components/table/data-table";
 import { loansListColumns } from "@/components/table/loan-list/loans-list-columms";
 import TablePagination from "@/components/table/table-pagination";
@@ -7,6 +8,7 @@ import { getAllLoansRequest } from "@/lib/actions/actions-loans-request";
 import { LOANS_STATUS } from "@/lib/data";
 import { LoanStatus } from "@prisma/client";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
 const AdminBorrowersPage = async ({
   searchParams,
@@ -21,7 +23,7 @@ const AdminBorrowersPage = async ({
   const validStatuses = Object.values(LoanStatus) as string[];
   if (status && !validStatuses.includes(status)) return notFound();
 
-  const { count, data } = await getAllLoansRequest(
+  const loansListPromise = getAllLoansRequest(
     p,
     search,
     status as LoanStatus | undefined,
@@ -32,10 +34,28 @@ const AdminBorrowersPage = async ({
       <ContainerSearchForm categoriesData={LOANS_STATUS} name="status">
         <DialogCreateLoansList />
       </ContainerSearchForm>
-      <DataTable columns={loansListColumns} data={data ?? []} />
-      <TablePagination currentPage={p} count={count ?? 0} />
+      <Suspense fallback={<TableLoadingSkeleton />}>
+        <DataTableWrapper loansListPromise={loansListPromise} page={p} />
+      </Suspense>
     </div>
   );
 };
+
+async function DataTableWrapper({
+  loansListPromise,
+  page,
+}: {
+  loansListPromise: ReturnType<typeof getAllLoansRequest>;
+  page: number;
+}) {
+  const loansList = await loansListPromise;
+
+  return (
+    <>
+      <DataTable columns={loansListColumns} data={loansList?.data ?? []} />
+      <TablePagination currentPage={page} count={loansList?.count ?? 0} />
+    </>
+  );
+}
 
 export default AdminBorrowersPage;
